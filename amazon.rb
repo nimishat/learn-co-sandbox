e9628791a1a41c305821c1ee79de6b5b09187b60
nimishat <nimishathakkar83@gmail.com>
2018-07-11 17:20:47 +0000

    Done.

---
+++ amazon.rb
@@ -0,0 +1,19 @@
+#Challenge 1
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+new_cart= []
+cart_item_prices.each do |x|
+  new_price = new_price = x * 1.07
+  new_price = new_price.round(2)
+  new_cart << new_price
+end
+puts new_cart
+
+#Challenge 2
+expensive = []
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+cart_item_prices.each do |item|
+if item > 15
+expensive << item
+  end 
+end 
+puts expensive
