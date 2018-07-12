010175fc1a06ba30cda6848e875324d17403ec6c
nimishat <nimishathakkar83@gmail.com>
2018-07-12 15:38:15 +0000

    Done.

--- evenOddGame.rb
+++ evenOddGame.rb
@@ -1,10 +1,12 @@
 #input a number from user and print to screen whether it is even or odd. Then, play again.
 
-puts "Let's play a game! Enter a number."
-number = gets.chomp
+loop do 
+  puts "Let's play a game! Enter a number."
+  number = gets.chomp
 
 if number.to_i % 2 == 0 
   puts "Your number is even!"
 else
-  "Your number is odd!"
+  puts "Your number is odd!"
+  end
 end
